e3520cae5293c0335341a6bad88f928e68c110d7
Manish Garg <manishgargpro@gmail.com>
2017-10-09 19:45:55 -0400

    added auth token verification for api calls, and initialized adding users to mysql database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var path = require("path");
 var exphbs = require("express-handlebars");
 
 var app = express();
@@ -11,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(cookieParser());
 
 app.use(express.static("public"));
 
@@ -25,7 +28,7 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   console.log("Database synced")
   app.listen(PORT, function() {
     console.log("Listening on PORT " + PORT);
